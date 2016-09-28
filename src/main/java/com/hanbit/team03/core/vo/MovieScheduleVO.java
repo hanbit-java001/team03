f4371277367401a8a2d9b934a96d295443edfe05
hb5012 <hb5012@hb5012>
2016-09-28 14:42:56 +0900

    CinemaVO, MovieScheduleVO, MovieVO, ReserveMovieVO, UserVO

--- src/main/java/com/hanbit/team03/core/vo/MovieScheduleVO.java
+++ src/main/java/com/hanbit/team03/core/vo/MovieScheduleVO.java
@@ -1,5 +1,43 @@
 package com.hanbit.team03.core.vo;
 
 public class MovieScheduleVO {
+	private int theaterId;
+	private String timeId;
+	private int movieId;
+	private String startTime;
+	private String endTime;
+
+
+	public int getTheaterId() {
+		return theaterId;
+	}
+	public void setTheaterId(int theaterId) {
+		this.theaterId = theaterId;
+	}
+	public String getTimeId() {
+		return timeId;
+	}
+	public void setTimeId(String timeId) {
+		this.timeId = timeId;
+	}
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
 
 }
--- src/main/java/com/hanbit/team03/core/vo/ReserveMovieVO.java
+++ src/main/java/com/hanbit/team03/core/vo/ReserveMovieVO.java
@@ -1,5 +1,43 @@
 package com.hanbit.team03.core.vo;
 
 public class ReserveMovieVO {
+	private int reserveId;
+	private int theaterId;
+	private String timeId;
+	private int seatId;
+	private String userId;
+
+
+	public int getReserveId() {
+		return reserveId;
+	}
+	public void setReserveId(int reserveId) {
+		this.reserveId = reserveId;
+	}
+	public int getTheaterId() {
+		return theaterId;
+	}
+	public void setTheaterId(int theaterId) {
+		this.theaterId = theaterId;
+	}
+	public String getTimeId() {
+		return timeId;
+	}
+	public void setTimeId(String timeId) {
+		this.timeId = timeId;
+	}
+	public int getSeatId() {
+		return seatId;
+	}
+	public void setSeatId(int seatId) {
+		this.seatId = seatId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
 
 }
