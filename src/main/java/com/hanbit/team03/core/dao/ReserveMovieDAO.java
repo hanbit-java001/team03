c15626511393b0d4082ea6e624f6dd4b298fd481
hyoungjinBae <frost_wyrm@naver.com>
2016-09-28 15:13:13 +0900

    ReserveMovieDAO, ReserveMovieService, reserveMovie.mxl

--- src/main/java/com/hanbit/team03/core/dao/ReserveMovieDAO.java
+++ src/main/java/com/hanbit/team03/core/dao/ReserveMovieDAO.java
@@ -1,5 +1,39 @@
 package com.hanbit.team03.core.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.hanbit.team03.core.vo.ReserveMovieVO;
+
+@Repository
 public class ReserveMovieDAO {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReserveMovieDAO.class);
+
+	@Autowired
+	private SqlSession sqlSession;
+
+	public int insertReservation(ReserveMovieVO reserveMovieVO){
+		return 0;
+	}
+
+	public int deleteReservation(int reserveId){
+		return 0;
+	}
+
+	public ReserveMovieVO selectReservation(int reserveId){
+		return null;
+	}
+
+	public List<ReserveMovieVO> selectReservations(String userId){
+		return null;
+	}
+
+
 
 }
--- src/main/java/com/hanbit/team03/core/service/ReserveMovieService.java
+++ src/main/java/com/hanbit/team03/core/service/ReserveMovieService.java
@@ -2,24 +2,45 @@ package com.hanbit.team03.core.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hanbit.team03.core.dao.ReserveMovieDAO;
 import com.hanbit.team03.core.vo.ReserveMovieVO;
 
 @Service
 public class ReserveMovieService {
 
+	@Autowired
+	private ReserveMovieDAO reserveMovieDAO;
 
-	public int reserveMovie(){
-		return 0;
+	public int reserveMovie(ReserveMovieVO reserveMovieVO){
+
+		int resultReservation = reserveMovieDAO.insertReservation(reserveMovieVO);
+
+		if(resultReservation <= 0){
+			throw new RuntimeException("예매가 정상적으로 이루어지지 않았습니다.");
+		}
+
+		return reserveMovieVO.getReserveId();
 	}
-	public boolean removeReservation(){
-		return false;
+
+
+	public int removeReservation(int reserveId){
+		int resultRemoveReservation = reserveMovieDAO.deleteReservation(reserveId);
+
+		if(resultRemoveReservation <= 0){
+			throw new RuntimeException("예매취소가 정상적으로 이루어지지 않았습니다.");
+		}
+
+		return reserveId;
 	}
+
 
 	public List<ReserveMovieVO> getReservations(){
 		return null;
 	}
+
 
 	public ReserveMovieVO getDetailReservation(){
 		return null;
